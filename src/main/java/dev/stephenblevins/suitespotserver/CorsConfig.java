c56e26768a7f6df6a52eb7207f76ec06cf92ebc3
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-05-17 15:13:01 -0400

    trial and error

--- src/main/java/dev/stephenblevins/suitespotserver/CorsConfig.java
+++ src/main/java/dev/stephenblevins/suitespotserver/CorsConfig.java
@@ -1,20 +1,28 @@
 package dev.stephenblevins.suitespotserver;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "*",
+                                "https://sblevins-dev.github.io/suite-spot-client/",
+                                "http://suite-spot-client.s3-website-us-east-1.amazonaws.com/"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
--- src/main/java/dev/stephenblevins/suitespotserver/booking/locations/LocationController.java
+++ src/main/java/dev/stephenblevins/suitespotserver/booking/locations/LocationController.java
@@ -1,10 +1,7 @@
 package dev.stephenblevins.suitespotserver.booking.locations;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
